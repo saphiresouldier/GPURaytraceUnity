ca6250596fddaa42f03019087ac4e45fecaf96bb
Phil Gosch <phil@saphirestudio.at>
2018-06-02 01:33:08 +0200

    Added scene definition via cs script

--- Assets/Scripts/RaytracingController.cs
+++ Assets/Scripts/RaytracingController.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+struct Sphere
+{
+    public Vector3 position;
+    public float radius;
+    public Vector3 albedo;
+    public Vector3 specular;
+};
+
 public class RaytracingController : MonoBehaviour {
 
     public ComputeShader RayTraceShader;
     public Texture SkyboxTex;
     public Light DirectionalLight;
+
+    public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    public uint SpheresMax = 100;
+    public float SpherePlacementRadius = 100.0f;
 
     [SerializeField]
     private float _skyboxMultiplicator = 1.0f;
@@ -14,6 +26,7 @@ public class RaytracingController : MonoBehaviour {
     private Camera _camera;
     private uint _currentSample = 0;
     private Material _addMaterial;
+    private ComputeBuffer _sphereBuffer;
 
     public float SkyboxMultiplicator
     {
@@ -27,6 +40,12 @@ public class RaytracingController : MonoBehaviour {
     private void Awake()
     {
         _camera = GetComponent<Camera>();
+    }
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetupSphereScene();
     }
 
     private void Update()
@@ -44,6 +63,46 @@ public class RaytracingController : MonoBehaviour {
         }
     }
 
+    private void SetupSphereScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                {
+                    goto SkipSphere;
+                }
+                    
+            }
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.5f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+            
+            // Add the sphere to the list
+            spheres.Add(sphere);
+
+        SkipSphere:
+            continue;
+        }
+        // Assign to compute buffer, 40 is byte size of sphere struct in memory
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40);
+        _sphereBuffer.SetData(spheres);
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         SetShaderParameters();
@@ -57,9 +116,9 @@ public class RaytracingController : MonoBehaviour {
         RayTraceShader.SetTexture(0, "_SkyboxTex", SkyboxTex);
         RayTraceShader.SetFloat("_SkyboxTexFactor", _skyboxMultiplicator);
         RayTraceShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
-
         Vector3 l = DirectionalLight.transform.forward;
         RayTraceShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+        RayTraceShader.SetBuffer(0, "_Spheres", _sphereBuffer);
     }
 
     private void Render(RenderTexture dest)
